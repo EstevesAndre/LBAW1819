0ddf9ef26729095f0d06175432307c3f9558744d
EstevesAndre <estevesandre1@hotmail.com>
2019-02-26 14:41:55 +0000

    test

--- pages/home.php
+++ pages/home.php
@@ -10,17 +10,16 @@
     function home_page()
     {
 ?>
-        <div class="bg"></div>
-        <div class="container">
+        <div class="container text-center align-middle">
             <div class="row">
-                <div class="col-sm">
-                One of three columns
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                    signup form
                 </div>
-                <div class="col-sm">
-                One of three columns
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                    <img src="../assets/logo.png" class="img-fluid" max-width="100%" alt="Responsive image">
                 </div>
-                <div class="col-sm">
-                One of three columns
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                    login form
                 </div>
             </div>
         </div>
