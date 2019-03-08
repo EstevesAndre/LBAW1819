a1592a2d948f760a1498e39266e24dfaf134170c
EstevesAndre <estevesandre1@hotmail.com>
2019-03-08 19:06:44 +0000

    not in use

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,9 @@
 /*
 Update Active Friend On Chat  
 */
+
+// NOT in USE
+
 let friends = document.getElementById('chat-friends');
 let active_friend = document.getElementById('active-chat-header');
 if(friends) friends.addEventListener('click', updateActiveChat);
@@ -9,3 +12,5 @@ if(friends) friends.addEventListener('click', updateActiveChat);
 function updateActiveChat(e) {
     active_friend.innerHTML = e.target.innerHTML;
 }
+
+// ^ NOT IN USE
