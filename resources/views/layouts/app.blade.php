ed1d70eea81f27bce58eb0fbe3db81317b0d623b
EstevesAndre <estevesandre1@hotmail.com>
2019-04-20 14:24:52 +0100

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,19 @@
     <body>
         <main>
         <header>
+            <nav class="bg-secondary fixed-top navbar navbar-expand-lg fixed-nav">
+                @if (Auth::check())
+                    <a class="navbar-brand" href="../index.html">
+                        <img src="../assets/logoWhite.png" width="40" height="40" alt="icon">
+                        <a class="nav-link index-nav" href="../index.html">AlterEgo</a>
+                    </a>
+                @else
+                    <a class="navbar-brand" href="../home.html">
+                        <img src="../assets/logoWhite.png" width="40" height="40" alt="icon">
+                        <a class="nav-link index-nav" href="{{ url('/cards') }}">AlterEgo</a>
+                    </a>
+                @endif
+            </nav>
             <h1><a href="{{ url('/cards') }}">Thingy!</a></h1>
             @if (Auth::check())
             <a class="button" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
@@ -38,3 +51,105 @@
         </main>
     </body>
 </html>
+
+<nav class="bg-secondary fixed-top navbar navbar-expand-lg fixed-nav">
+        <a class="navbar-brand" href="../index.html">
+            <img src="../assets/logoWhite.png" width="40" height="40" alt="icon">
+            <a class="nav-link index-nav" href="../index.html">AlterEgo</a>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars hamburger-icon"></i>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class=" navbar-nav mr-auto">
+                <li class="nav-item align-self-center">
+                    <a class="nav-link index-nav active" href="../pages/about.html">Get Started!</a>
+                </li>
+                <li class="nav-item align-self-center">
+                    <a class="nav-link index-nav" href="../pages/faqs.html">FAQs</a>
+                </li>
+            </ul>
+            <div class="d-flex justify-content-center mr-2">
+                <a href="../pages/login.html" class="btn btn-link bg-dark nav-link index-nav">
+                    Login
+                </a>
+            </div>
+        </div>
+    </nav>
+
+    <nav class="bg-secondary fixed-top navbar navbar-expand-lg fixed-nav">
+        <a class="navbar-brand" href="../pages/home.html">
+            <img src="../assets/logoWhite.png" width="40" height="40" alt="icon">
+            <a class="nav-link index-nav" href="../pages/home.html">AlterEgo</a>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars hamburger-icon"></i>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item align-self-center">
+                    <a class="nav-link index-nav active" href="../pages/profile.html">Profile</a>
+                </li>
+                <li class="nav-item align-self-center">
+                    <a class="nav-link index-nav" href="../pages/clan.html">Clan</a>
+                </li>
+                <li class="nav-item align-self-center">
+                    <a class="nav-link index-nav" href="../pages/leaderboard.html">Leaderboards</a>
+                </li>
+            </ul>
+            <div class="d-flex justify-content-center mr-2">
+                <div class="searchbar">
+                    <input class="search_input" type="text" name="" placeholder="Search...">
+                    <a href="" class="search_icon"><i class="fas fa-search"></i></a>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center mx-2 align-items-center">
+                <div class="btn-group my-2">
+                    <button type="button" class="btn btn-secondary rounded" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false"><i class="fas fa-user-friends"></i></button>
+                    <div class="dropdown-menu bg-secondary">
+                        <a class="no-hover index-nav" href="../pages/friends.html"><button class="dropdown-item dropdown-navbar" type="button">Go to Friend Requests</button></a>
+                    </div>
+                </div>
+                <div class="btn-group mr-1">
+                    <button type="button" class="btn btn-secondary rounded" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false"><i class="far fa-envelope"></i></button>
+                    <div class="dropdown-menu bg-secondary">
+                        <a class="no-hover index-nav" href="../pages/chat.html"><button class="dropdown-item dropdown-navbar" type="button">Go to Chat</button></a>
+                    </div>
+                </div>
+                <div class="btn-group mr-3 my-2">
+                    <button type="button" class="btn btn-secondary rounded" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false"><i class="far fa-bell"></i></button>
+                    <div class="dropdown-menu bg-secondary">
+                        <a class="no-hover index-nav" href="../pages/home.html"><button class="dropdown-item dropdown-navbar" type="button">Go to Notifications</button></a>
+                    </div>
+                </div>
+
+                <a href="../pages/profile.html"><img width="40" class="img-fluid border rounded-circle mr-2"
+                        src="../assets/logo.png" alt="User"></a>
+                <a href="../pages/profile.html" class="m-0 no-hover index-nav">André Esteves</a>
+                <div class="btn-group ml-2 my-2">
+                    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false"></button>
+                    <div class="dropdown-menu dropdown-menu-right bg-secondary">
+                        <a class="no-hover index-nav" href="../pages/createClan.html">
+                            <button class="dropdown-item dropdown-navbar" type="button">Create Clan</button>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="no-hover index-nav" href="../pages/administrator.html">
+                            <button class="dropdown-item dropdown-navbar" type="button">Administrator</button>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="no-hover index-nav" href="../index.html">
+                            <button class="dropdown-item dropdown-navbar" type="button">Log out</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
