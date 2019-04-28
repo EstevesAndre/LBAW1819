6e7eab3116a1091b94dc8b2d0fa6dddca930c920
EstevesAndre <estevesandre1@hotmail.com>
2019-04-28 16:02:51 +0100

    corrected post functions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,16 +13,6 @@ use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
-    /**
-     * Shows the home page
-     */
-    public function showPage()
-    {
-        if (!Auth::check()) return redirect('/login');
-        
-        return view('pages.home');
-    }
-
     public function show($id)
     {
         if (!Auth::check()) return redirect('/login');
