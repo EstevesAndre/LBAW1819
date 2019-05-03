740b38ad90007d9fae730f99712eb9c50b9a3f31
EstevesAndre <estevesandre1@hotmail.com>
2019-05-03 23:03:15 +0100

    add like and delete like  json completed

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,20 @@ function addEventListeners() {
     [].forEach.call(addLike, function(like) {
         like.addEventListener('click', sendAddLikeRequest);
     });
+
+    let friendList = document.querySelectorAll('#friends>ul>li>button, #members>ul>li>button, #leaderboard>ol>button');
+    [].forEach.call(friendList, function(friend) {
+        friend.addEventListener('click', function() {
+            window.location.href = this.getAttribute('data-id');
+        });
+    });
+
+    /*$("div.post").hover(function() {
+        $post_id = $(this)[0].getAttribute('data-id');
+        addEventListener('click', function() {
+            window.location.href = "/post/" + $post_id;
+        })
+    });*/
 }
 
 function encodeForAjax(data) {
@@ -29,14 +43,16 @@ function sendAjaxRequest(method, url, data, handler) {
 
 // Requests
 function sendDeleteLikeRequest() {
-    console.log("delete");
+    console.log("Like delete request");
     let post_id = this.closest('div.post').getAttribute('data-id');
 
     sendAjaxRequest('delete', '/api/like/' + post_id, null, deletedLikeHandler);
+
+    event.preventDefault();
 }
 
 function sendAddLikeRequest(event) {
-    console.log("add");
+    console.log("Like add request");
 
     let post_id = this.closest('div.post').getAttribute('data-id');
 
@@ -47,25 +63,32 @@ function sendAddLikeRequest(event) {
 
 // Handlers
 function deletedLikeHandler() {
-    console.log("delete2");
-    console.log(JSON.parse(this.responseText));
+    
+    let like = JSON.parse(this.responseText);
+    console.log("Like delete - status: " + this.status + ", [1-removed;2-error]: " + like.status);
 
-    let thumbs_up = document.querySelector('div.post[data-id="' + 1 + '"]>div>div>ul>li>a');
+    let thumbs_up = document.querySelector('div.post[data-id="' + like.post_id + '"]>div>div>ul.fst>li>a');
     thumbs_up.innerHTML = "<i class='fa fa-thumbs-up'></i>";
+
+    let numLikes = document.querySelector('div.post[data-id="' + like.post_id + '"]>div>div>ul.fst>li+li>a>span');
+    let value = numLikes.innerHTML;
+    value--;
+    numLikes.innerHTML = value;
 
     addEventListeners();    
 }
 
 function addedLikeHandler() {
-    console.log("add2");
-    
-    if(this.status != 200) window.location = '/';
+    console.log("Like add - status: " + this.status);
+
     let like = JSON.parse(this.responseText);
 
-   // let new_like = createLike(like);
-
-    let thumbs_up = document.querySelector('div.post[data-id="' + like.post_id + '"]>div>div>ul>li>a');
+    let thumbs_up = document.querySelector('div.post[data-id="' + like.post_id + '"]>div>div>ul.fst>li>a');
     thumbs_up.innerHTML = "<i class='fa fa-thumbs-up active'></i>";
+    let numLikes = document.querySelector('div.post[data-id="' + like.post_id + '"]>div>div>ul.fst>li+li>a>span');
+    let value = numLikes.innerHTML;
+    value++;
+    numLikes.innerHTML = value;
 
     addEventListeners();
 }
