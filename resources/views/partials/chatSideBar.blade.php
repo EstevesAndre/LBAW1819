1329dc3e8590f03f1d2386ddf1cd18b32fdd4cad
luisdiogo98 <luisdiogo98@hotmail.com>
2019-05-21 11:47:44 +0100

    Updated profile pics in chat

--- resources/views/partials/chatSideBar.blade.php
+++ resources/views/partials/chatSideBar.blade.php
@@ -11,7 +11,7 @@
         <div class="height-45 scroolable">
             <div class="list-group text-left" id="list-tab" role="tablist">
                 <a class="friend-list list-group-item list-group-item-action active" id="{{ $friends[0]->id }}" data-toggle="list" href="#list-{{ $friends[0]->id }}" aria-controls="{{ $friends[0]->id }}">
-                    <img src="{{ asset('assets/logo.png') }}" alt="logo" width="25" class="border bg-warning img-fluid rounded-circle">
+                    <img src="{{ asset('assets/avatars/'.$friends[0]->race.'_'.$friends[0]->class.'_'.$friends[0]->gender.'.bmp') }}" alt="logo" width="25" class="mr-2 border bg-warning img-fluid rounded-circle">
                 {{ $friends[0]->name }}
                 </a>
                 @each('partials.chatFriend', $friends->slice(1)->take(14), 'user')
@@ -24,8 +24,8 @@
     <div class="border-left height-45 friend-chat">
         <div class="py-3 px-3 border rounded text-left tab-content chat-content" id="nav-tabContent">
             <div class="tab-pane fade show active" id="list-1" role="tabpanel" aria-labelledby="list-1-list">
-                <img src="{{ asset('assets/logo.png') }}" alt="logo" width="25"
-                    class="border bg-warning img-fluid rounded-circle">
+                <img src="{{ asset('assets/avatars/'.$friends[0]->race.'_'.$friends[0]->class.'_'.$friends[0]->gender.'.bmp') }}" alt="logo" width="25"
+                    class="mr-2 border bg-warning img-fluid rounded-circle">
                 <a href="/user/{{ $friends[0]->username }}">{{ $friends[0]->name }}</a>
             </div>
         </div>
