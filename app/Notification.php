7858c373f94f9ede6fa4691652eeab9aa6af4bd5
EstevesAndre <estevesandre1@hotmail.com>
2019-06-01 22:31:42 +0100

    Created notification model

---
+++ app/Notification.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    // Don't add create and update timestamps in database.
+    public $timestamps  = false;
+    protected $table = 'notifications';
+   
+
+    public function userLiked() {
+        return $this->belongsTo('App\User', 'like_user_id', 'id');
+    }
+
+    public function userShared() {
+        return $this->belongsTo('App\User', 'share_user_id', 'id');
+    }
+
+    
+
+}
