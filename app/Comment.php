d7d3fff7b57e2c5085765adc1f46e8dac144f728
EstevesAndre <estevesandre1@hotmail.com>
2019-06-01 22:31:07 +0100

    added get message owner

--- app/Comment.php
+++ app/Comment.php
@@ -16,4 +16,8 @@ class Comment extends Model
     public function post() {
         return $this->belongsTo('App\Post', 'id', 'post_id');
     }
+
+    public function user() {
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
 }
