5bfd5101736211bf4373299d11fab7c4ee007095
luisdiogo98 <luisdiogo98@hotmail.com>
2019-06-03 14:18:43 +0100

    Added tabs to administrator

--- resources/views/pages/administrator.blade.php
+++ resources/views/pages/administrator.blade.php
@@ -4,121 +4,62 @@
 <br>
 <br>
 <div class="container mt-5">
-        <h1 class="text-center"><b>Administrator's Page</b></h1>
-        <div class="row admin-content py-3 px-3">
-            <div class="col-md-3 rt-border mb-3">
-                <ul class="nav navbar-nav side-nav fixed admin-sidebar">
-                    <li class="my-2">
-                        <a href="manageUsers.html"><i class="fas fa-user"></i> Manage Users</a>
-                    </li>
-                    <li class="my-2">
-                        <a href="manageClans.html"><i class="fas fa-users"></i> Manage Clans</a>
-                    </li>
-                    <li class="my-2">
-                        <a href="manageAdministrators.html"><i class="fas fa-user-shield"></i> Manage Administrators</a>
-                    </li>
-                </ul>
+    <h1 class="text-center"><b>Administrator's Page</b></h1>
+    <div class="row admin-content py-3 px-3">
+    <button type="button" class="float-right border-0 btn btn-default btn-circle" data-toggle="modal" data-target="#clan_helpModal">
+            <i class="fas fa-question-circle"></i>
+    </button>
+    <div class="clan-page-info">
+        <ul class="mt-5 nav nav-tabs" id="clan-tabs" role="tablist">
+            <li class="nav-item">
+                <a class="tab-title nav-link active" id="report-tab" data-toggle="tab" href="#reports" role="tab"
+                    aria-controls="reports" aria-selected="true">Reports</a>
+            </li>
+            <li class="nav-item">
+                <a class="tab-title nav-link" id="users-tab" data-toggle="tab" href="#users" role="tab"
+                    aria-controls="users" aria-selected="false">Users</a>
+            </li>
+            <li class="nav-item">
+                <a class="tab-title nav-link" id="clans-tab" data-toggle="tab" href="#clans"
+                    role="tab" aria-controls="clans" aria-selected="false">Clans</a>
+            </li>
+            <li class="nav-item">
+                <a class="tab-title nav-link" id="admins-tab" data-toggle="tab" href="#admins"
+                    role="tab" aria-controls="admins" aria-selected="false">Administrators</a>
+            </li>
+        </ul>     
+        <div class="mt-4 tab-content" id="content">
+            <div class="text-left tab-pane fade active show" id="reports" role="tabpanel" aria-labelledby="reports-tab">
+                
             </div>
-
-            <div class="modal fade" id="reportModal" tabindex="-1" role="dialog" aria-labelledby="reportModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="reportModalLabel">Ignore report</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <p>Are you sure that you want to ignore this report?</p>
-                            <div class="float-right">
-                                <button type="button" class="btn btn-success">Yes</button>
-                                <button type="button" class="btn btn-danger">No</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="tab-pane fade" id="users" role="tabpanel" aria-labelledby="users-tab">
+                
             </div>
-
-            <div class="float-right col-md-9">
-                <div class="float-right mt-4 pr-2 dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Filter By
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-right text-center" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="#">User</a>
-                        <a class="dropdown-item" href="#">Clan</a>
-                    </div>
-                </div>
-                <div class="text-left mt-4">
-                    <h3 class="mb-4">Newest Reports</h3>
-                    <ul class="report-list pl-0">
-                        <li class="shadow-sm border">
-                            <div class="py-2 px-3 float-right">
-                                <button type="button" class="btn btn-link" data-toggle="modal" data-target="#reportModal">
-                                    <i class="fas fa-times color-black standard-text"></i>
-                                </button>
-                            </div>
-                            <p class="px-3 mx-2 mt-4">André Esteves reported Francisco Filipe</p>
-                            <div class="row tags text-center pb-4 px-5">
-                                <div class="col-12 col-sm-5 report-tag">User</div>
-                                <div class="col-12 col-sm-5 report-tag">Abusive content</div>
-                            </div>
-                        </li>
-                        <li class="shadow-sm border">
-                            <div class="py-2 px-3 float-right">
-                                <button type="button" class="btn btn-link" data-toggle="modal" data-target="#reportModal">
-                                    <i class="fas fa-times color-black standard-text"></i>
-                                </button>
-                            </div>
-                            <p class="px-3 mx-2 mt-4">André Esteves reported Francisco Filipe</p>
-                            <div class="row tags text-center pb-4 px-5">
-                                <div class="col-12 col-sm-5 report-tag">User</div>
-                                <div class="col-12 col-sm-5 report-tag">Abusive content</div>
-                            </div>
-                        </li>
-                        <li class="shadow-sm border">
-                            <div class="py-2 px-3 float-right">
-                                <button type="button" class="btn btn-link" data-toggle="modal" data-target="#reportModal">
-                                    <i class="fas fa-times color-black standard-text"></i>
-                                </button>
-                            </div>
-                            <p class="px-3 mx-2 mt-4">André Esteves reported PT-Pokemons</p>
-                            <div class="row tags text-center pb-4 px-5">
-                                <div class="col-12 col-sm-5 report-tag">Clan</div>
-                            </div>
-                        </li>
-                        <li class="shadow-sm border">
-                            <div class="py-2 px-3 float-right">
-                                <button type="button" class="btn btn-link" data-toggle="modal" data-target="#reportModal">
-                                    <i class="fas fa-times color-black standard-text"></i>
-                                </button>
-                            </div>
-                            <p class="px-3 mx-2 mt-4">João banned Luís Silva</p>
-                            <div class="row tags text-center pb-4 px-5">
-                                <div class="col-12 col-sm-5 report-tag">User</div>
-                                <div class="col-12 col-sm-5 report-tag">Racism</div>
-                                <div class="col-12 col-sm-5 report-tag">Abusive content</div>
-                            </div>
-                        </li>
-                        <li class="shadow-sm border">
-                            <div class="py-2 px-3 float-right">
-                                <button type="button" class="btn btn-link" data-toggle="modal" data-target="#reportModal">
-                                    <i class="fas fa-times color-black standard-text"></i>
-                                </button>
-                            </div>
-                            <p class="px-3 mx-2 mt-4">Diogo banned Bruno's Clan</p>
-                            <div class="row tags text-center pb-4 px-5">
-                                <div class="col-12 col-sm-5 report-tag">Clan</div>
-                                <div class="col-12 col-sm-5 report-tag">Inappropriate behaviour</div>
-                            </div>
-                        </li>
-
-                    </ul>
-                </div>
+            <div class="tab-pane fade" id="clans" role="tabpanel" aria-labelledby="clans-tab">
+                
+            </div>
+            <div class="tab-pane fade" id="admins" role="tabpanel" aria-labelledby="admins-tab">
+                
             </div>
         </div>
+    </div>
+    @include('partials.chatSideBar', ['friends' => Auth::user()->friends() ])
+</div>
+<!-- Modal -->
+<div class="modal fade" id="clan_helpModal" tabindex="-1" role="dialog" aria-labelledby="clan_helpModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="clan_helpModalLabel">Clan Help</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            This is the administation page.
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
+
