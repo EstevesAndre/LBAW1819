f7aa65deb110b9b1c179a7405d0e36619f448ee1
EstevesAndre <estevesandre1@hotmail.com>
2019-06-04 17:31:52 +0100

    friend posts with eloquent

--- app/Http/Controllers/PrivateController.php
+++ app/Http/Controllers/PrivateController.php
@@ -21,22 +21,14 @@ class PrivateController extends Controller
     {
         if (!Auth::check()) return redirect('/login');
 
-        $friendPosts = DB::select('SELECT id FROM posts 
-                                    WHERE (posts.user_id IN 
-                                        (SELECT sender FROM requests
-                                            WHERE receiver = :ID AND has_accepted = true AND type = \'friendRequest\')
-                                        OR posts.user_id IN
-                                        (SELECT receiver FROM requests
-                                            WHERE sender = :ID AND has_accepted = true AND type = \'friendRequest\')
-                                        OR posts.user_id = :ID)
-                                    AND clan_id IS NULL
-                                    ORDER BY posts.date DESC'
-                                    , ['ID' => Auth::user()->id]);
-                
-        $posts = [];
-        foreach ($friendPosts as $post) {
-            array_push($posts , Post::find($post->id));
-        }
+        $friends = Auth::user()->friends()->get();
+        
+        $idPosts = [];
+        foreach($friends as $friend) 
+            foreach($friend->posts()->get() as $post)
+                array_push($idPosts, $post->id);
+        
+        $posts = Post::whereIn('id', $idPosts)->orderBy('date', 'DESC')->get();
         
         return view('pages.home', ['posts' => $posts]);
     }
@@ -45,10 +37,7 @@ class PrivateController extends Controller
     {
         if (!Auth::check()) return redirect('/login');
 
-        $allUsers = DB::table('users')
-            ->select('username', 'name', 'xp', 'race', 'class', 'gender')
-            ->orderBy('xp', 'DESC')
-            ->get();
+        $allUsers = User::all();
 
         $userClan = Auth::user()->clan()->get()[0];
 
