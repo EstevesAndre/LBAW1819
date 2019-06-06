82c116a534cd48d153b2807a6f2bb078a1349284
EstevesAndre <estevesandre1@hotmail.com>
2019-06-06 15:40:58 +0100

    fixed

--- resources/views/partials/requestSent.blade.php
+++ resources/views/partials/requestSent.blade.php
@@ -1,12 +1,12 @@
-<li class="friend-cancel-rp p-2 ml-3" data-id='{{ $request->receiver()->get()[0]->id}}'>
+<li class="friend-cancel-rp py-2 px-2 ml-3" data-id='{{ $request->receiver()->get()[0]->id}}'>
     <div class="d-flex align-items-center row">
         <div class="pl-3 col-2 col-sm-2 col-md-1 friend-img">
-            <img width="40" class="border bg-warning img-fluid rounded-circle border" src="{{ asset('assets/avatars/'.$request->receiver()->get()[0]->race.'_'.$request->receiver()->get()[0]->class.'_'.$request->receiver()->get()[0]->gender.'.bmp') }}" alt="User">
+            <img width="60" class="border bg-warning img-fluid rounded-circle border" src="{{ asset('assets/avatars/'.$request->receiver()->get()[0]->race.'_'.$request->receiver()->get()[0]->class.'_'.$request->receiver()->get()[0]->gender.'.bmp') }}" alt="User">
         </div>
         <div class="col-6 col-sm-5 col-md-6 pr-1 text-left"><a class="no-hover standard-text" href="/user/{{ $request->receiver()->get()[0]->username}}">{{ $request->receiver()->get()[0]->name}}</a></div>
         <div class="col-3 col-sm-4 col-md-4 px-0 text-right">
-            <button type="button" class="friend-cancel col-sm-12 mt-5 btn btn-danger" data-id="{{$request->receiver()->get()[0]->id}}"> 
-                    Cancel Request <i class="fas fa-times"></i>
+            <button type="button" class="friend-cancel col-sm-12 btn btn-danger" data-id="{{$request->receiver()->get()[0]->id}}">
+                Cancel Request <i class="fas fa-times"></i>
             </button>
         </div>
     </div>
