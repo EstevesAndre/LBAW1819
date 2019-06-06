01ec83aa33647c4d7a71eee122cf9c6a7bfb3c1f
luisdiogo98 <luisdiogo98@hotmail.com>
2019-06-06 16:36:08 +0100

    Added additional checks to post view

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -58,7 +58,9 @@
                     <div id="posts-list">
                         @foreach($posts as $p)
                             @if($p->id != null)
-                                @include('partials.post', ['post' => $p])
+                                @if($p->clan_id == null || (!Auth::user()->clan()->get()->isEmpty() && $p->clan_id == Auth::user()->clan()->get()[0]->id))
+                                    @include('partials.post', ['post' => $p])
+                                @endif
                             @else
                                 @include('partials.share', ['share' => $p])
                             @endif
