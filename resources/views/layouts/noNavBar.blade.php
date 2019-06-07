53d630fe5187a9ec8e74a30bded1eef0cd01c9ee
EstevesAndre <estevesandre1@hotmail.com>
2019-06-07 11:40:58 +0100

    fixed others navbars

--- resources/views/layouts/noNavBar.blade.php
+++ resources/views/layouts/noNavBar.blade.php
@@ -18,14 +18,14 @@
         
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-        <script type="text/javascript">
+        <script>
             // Fix for Firefox autofocus CSS bug
             // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
         </script>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous" defer></script>
-        <script type="text/javascript" src={{ asset('js/bootstrap/bootstrap.min.js') }} defer></script>
-        <script type="text/javascript" src={{ asset('js/app.js') }} defer></script>
+        <script src={{ asset('js/bootstrap/bootstrap.min.js') }} defer></script>
+        <script src={{ asset('js/app.js') }} defer></script>
     </head>
     <body>
         <main class="h-100">
